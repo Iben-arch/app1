09e8b3bf08b754e0bd86ce5553bccb202d658c56
Iben <bourbok49@gmail.com>
2025-07-21 11:00:53 +0700

    Function Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-import './style.css'
+import React from 'react';
+import {Header, Footer, Content} from './func-components'
 
 function App() {
  return (
-  <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React & React Native</div>
-  </>
+  [<Header/>, <Content/>, <Footer/>]
  )
 }
 
