fbf09092bca97b318740f5aa1a3263ea0e8775bf
Iben <bourbok49@gmail.com>
2025-08-04 10:37:34 +0700

    calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {Header, Footer, Content} from './func-components'
-import Button from './class-components';
+import { Calculator2 } from './calculator';
 
 function App() {
- return <Button/>
+ return <Calculator2/>
 }
 
 export default App;
