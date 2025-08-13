68797f8406771377d515573817b7bcff9e79a307
Iben <bourbok49@gmail.com>
2025-08-13 14:05:20 +0700

    Refs-Array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { EventData2 } from './event-data';
+import RefsArray from './refs-array';
 
-function App() {
- return <EventData2/>
+export default function App() {
+    return <RefsArray/>
 }
-
-export default App;
